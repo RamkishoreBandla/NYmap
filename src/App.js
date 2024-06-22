6ece4c2f4336af87b77975b575f986b9686c28f2
RamkishoreBandla <bandlaramkishore090@gmail.com>
2024-06-21 20:04:50 -0400

    map added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from "./Map";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+
+
 
 function App() {
+
+  const [color,setColor] = useState("#ffff00");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col text-center">
+          <h2>New York Counties</h2>
+        </div>
+
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-4">
+          
+          <Card className="p-3">
+          <h3>Color</h3>
+          <p> Choose a color to apply to county</p>
+          <input type="color" value={color} onChange={(e)=>{setColor(e.target.value)}}/>
+          </Card>
+        </div>
+        <div className="col-md-8">
+          <Map color={color} />
+        </div>
+      </div>
     </div>
   );
 }
